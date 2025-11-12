89967892de936034c259735b612ad77a2e3b9030
Cedrik6245653 <6245653@cegepmontpetit.ca>
2025-11-12 16:33:23 -0500

    commit

--- src/scripts/Main.js
+++ src/scripts/Main.js
@@ -1,4 +1,5 @@
 import Icons from './utils/Icons.js';
+import Swiper from 'swiper/bundle';
 
 class Main {
   constructor() {
@@ -7,6 +8,10 @@ class Main {
 
   init() {
     Icons.load();
+    const swiper = new Swiper('.js-swiper', {
+      slidesPerView: 1,
+      pagination: { el: '.swiper-pagination' },
+    });
   }
 }
 new Main();
